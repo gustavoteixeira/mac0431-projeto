5b352f98d2798eae607b80022c18201d869e1df2
Gustavo Teixeira da Cunha Coelho <gustavotdaccoelho@gmail.com>
2013-05-18 03:34:30 -0300

    Finalmente o projeto funciona. Ele realiza uma iteração do que o desejado porém.

--- projeto.cc
+++ projeto.cc
@@ -44,7 +44,7 @@ class Vector2D {
 };
 
 struct part {
-    double x, y, vx, vy, carga, raio;
+    double x, y, vx, vy, lx, ly, carga, raio;
 };
 
 typedef struct part particula;
@@ -52,6 +52,7 @@ typedef struct part particula;
 int main(int argc, char* argv[]) {
     double x, y, distancia_sqr, distancia, forca = 0;
     int i, j, k, tid;
+    bool bounce = false;
     std::ifstream file(argv[1], std::ifstream::in);
     if(!file) return 1;
     int n_partic = 0;
@@ -71,63 +72,81 @@ int main(int argc, char* argv[]) {
     particula particulas[n_partic];
     Vector2D forcas[n_partic];
     int iterador[n_partic];
-    double lx[n_partic], ly[n_partic];
     for(i = 0; i < n_partic; ++i) {
         file >> particulas[i].x; file >> particulas[i].y;
-        file >> lx[i]; file >> ly[i];
+        file >> particulas[i].lx; file >> particulas[i].ly;
         file >> particulas[i].carga; file >> particulas[i].raio;
     }
     printf("%s %f %f %f %d %d\n", argv[1], c, epsilon, tau, iters, procs);
     printf("%d, %f, %f\n", n_partic, size_x, size_y);
-    for(i = 0; i < n_partic; ++i) {
+    for(i = 0; i < n_partic; ++i)
         printf("\t%f %f %f %f %f %f\n", particulas[i].x, particulas[i].y, particulas[i].vx, particulas[i].vy, particulas[i].carga,particulas[i].raio);
-        lx[i] = 0; ly[i] = 0;
-    }
+    printf("\n\n");
     
-    #pragma omp parallel private(lx, ly, tid)
+    #pragma omp parallel private(i, j, k, x, y, distancia, distancia_sqr, bounce, tid)
     {
-        tid=omp_get_thread_num();
+        tid = omp_get_thread_num();
+        x = 0; y = 0; distancia = 0; distancia_sqr = 0; k = 0; bounce = false;
         for(i = 0; i < iters; ++i) {
-            if(i%25000 == 0 || i > 2499900)
-                printf("Iteracao %d\n", i);
-            #pragma omp for private(x, y, distancia, distancia_sqr, k)
+            //printf("%d",tid);
+            //if(i%25000 == 0 || i > 2499900)
+            //    printf("Iteracao %d\n", i);
+            #pragma omp for
             for(j = 0; j < n_partic; ++j) {
                 forcas[j].x(0);
                 forcas[j].y(0);
-                particulas[j].vx = lx[j];
-                particulas[j].vy = ly[j];
+                particulas[j].vx = particulas[j].lx;
+                particulas[j].vy = particulas[j].ly;
                 for(k = 0; k < n_partic; ++k) {
-                    //#pragma omp critical
-                    //printf("-%d, %d-\n", j, k);
                     if(j == k)
                         continue;
-                    x = (particulas[j].x - particulas[k].x);
-                    y = (particulas[j].y - particulas[k].y);
+                    x = (particulas[k].x - particulas[j].x);
+                    y = (particulas[k].y - particulas[j].y);
                     distancia_sqr = x*x + y*y;
                     distancia = sqrt(distancia_sqr);
                     if(distancia > epsilon)
                         continue;
+                    if(distancia < particulas[k].raio+particulas[j].raio)
+                        continue;
                     forca = c/distancia_sqr;
-                    forcas[j].add((x/distancia_sqr)*forca, (y/distancia_sqr)*forca);
-                    lx[j] = particulas[j].vx + forcas[j].x();
-                    ly[j] = particulas[j].vy + forcas[j].y();
+                    forcas[j].add(-(x/distancia_sqr)*forca*tau*particulas[j].carga*particulas[k].carga, -(y/distancia_sqr)*forca*tau*particulas[k].carga*particulas[k].carga);
                 }
+                particulas[j].lx = particulas[j].vx + forcas[j].x();
+                particulas[j].ly = particulas[j].vy + forcas[j].y();
             }
             
-            #pragma omp barrier
+            /*if(tid == 0) {
+                printf("Iter %d:\n", i);
+                for(j = 0; j < n_partic; ++j)
+                    printf("\t%f %f %f %f %f %f\n", particulas[j].x, particulas[j].y, particulas[j].vx, particulas[j].vy, particulas[j].carga, particulas[j].raio);
+            }*/
+            //#pragma omp barrier
             #pragma omp for
             for(j = 0; j < n_partic; ++j) {
-                particulas[j].x += particulas[j].vx;
-                particulas[j].y += particulas[j].vy;
-                if(particulas[j].x > size_x)
+                particulas[j].x += particulas[j].vx*tau;
+                particulas[j].y += particulas[j].vy*tau;
+                if(particulas[j].x > size_x) {
                     particulas[j].x = 2*size_x - particulas[j].x;
-                else if(particulas[j].x < 0)
+                    bounce = true;
+                }
+                else if(particulas[j].x < 0) {
                     particulas[j].x *= -1;
+                    bounce = true;
+                }
                 
-                if(particulas[j].y > size_y)
+                if(particulas[j].y > size_y) {
                     particulas[j].y = 2*size_y - particulas[j].y;
-                else if(particulas[j].y < 0)
+                    bounce = true;
+                }
+                else if(particulas[j].y < 0) {
                     particulas[j].y *= -1;
+                    bounce = true;
+                }
+                if(bounce) {
+                    bounce = false;
+                    particulas[j].vx /= 2;
+                    particulas[j].vy /= 2;
+                }
             }
         }
     }
